4e2daf4b692cea737dfc802487be01e10182da9e
Liora Nijhof <liora@nijhofnet.nl>
2026-09-15 10:21:15 +0200

    oefenopdracht 2

--- Oefenopdrachten/Opdracht 2/sketch.js
+++ Oefenopdrachten/Opdracht 2/sketch.js
@@ -1,7 +1,30 @@
+let x = 100
+let y = 80
+let greeting = "Hello World!"
+let a = 20
+let b = 10
+let optellen = a + b
+let aftrekken = a - b
+let vermenigvuldigen = a * b
+let delen = a / b
+
+//variabelen enzo hierboven
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+
+  text(x, 20, 20);
+  text(greeting, 20, 60); //ding noemt het variable op (is geen string dus geen aanhalingstekens)
+
+  text("Optellen = " + optellen, 20, y); //text met y coordinaat y zodat ie kan veranderen enzo
+  noLoop(y = y+ 20); // noLoop stop de text van weg te rennen
+  text("Aftrekken = " + aftrekken, 20, y);
+  noLoop(y = y + 20);
+  text("Vermenigvuldigen = " + vermenigvuldigen, 20, y);
+  noLoop(y = y + 20);
+  text("Delen = " + delen, 20, y);
 }
--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,7 +1,83 @@
+let x = 0;
+let y = 0;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth - 20, windowHeight - 20);
 }
 
+
 function draw() {
-  background(220);
+  background(191, 136, 71);
+  drawingContext.shadowBlur = 0;
+
+  //sun
+  fill(252, 197, 103);
+  drawingContext.shadowBlur = 30;
+  drawingContext.shadowColor = "rgb(252, 210, 138)";
+
+  circle(x, 150 + y, 20);
+  
+  x = x + 1.5;
+
+  if (x < width / 2) {
+    y = y - 0.2;
+  } else {
+    y = y + 0.2;
+  }
+
+  if (x > width + 20) { 
+    x = -20;
+    y = 0;
+  }
+
+  drawingContext.shadowBlur = 0;
+
+  //mouintaonts
+  smooth();
+  strokeWeight(2);
+  stroke(120, 77, 38);
+  fill(120, 77, 38);
+  
+  beginShape();
+
+  vertex(0, 215);
+  vertex(60, 205);
+  vertex(150, 250);
+  vertex(300, 400);
+  vertex(0, 400);
+
+  endShape(CLOSE);
+
+  beginShape();
+
+  vertex(700, 400);
+  vertex(850, 200);
+  vertex(930, 190);
+  vertex(1030, 50);
+  vertex(1100, 30);
+  vertex(windowWidth, 0);
+  vertex(windowWidth, 400);
+
+  endShape(CLOSE);
+
+  stroke(153, 101, 54);
+  fill(153, 101, 54);
+
+  beginShape();
+
+  vertex(0, 320);
+  vertex(110, 300);
+  vertex(260, 220);
+  vertex(350, 240);
+  vertex(410, 300);
+  vertex(550, 400);
+  vertex(0, 400);
+
+  endShape();
+
+  //road
+  strokeWeight(0);
+  fill(138, 89, 45);
+  rect(0, windowHeight - 300, windowWidth, 300);
+
 }
